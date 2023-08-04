0ca395ef68eed96ae55a114a762cc6a97a62444e
Godlearningtocode <shivam.ayush.agarwal@gmail.com>
2023-08-05 01:34:42 +0530

    changed button sequence;added operate logic.

--- script.js
+++ script.js
@@ -1,16 +1,21 @@
+let firstNumber = "";
+let secondNumber = "";
+let operator = null;
+let shouldReset = false;
 let add = (firstNumber, secondNumber) => firstNumber + secondNumber;
 let subtract = (firstNumber, secondNumber) => firstNumber - secondNumber;
 let multiply = (firstNumber, secondNumber) => firstNumber * secondNumber; 
-let divide = (firstNumber, secondNumber) => firstNumber / secondNumber;
-const operators = {
-    add: '+',
-    subtract: '-',
-    multiply: '*',
-    divide: '/',
-};
-const operatorsValues = Object.values(operators);
-const firstOperand = "";
-const secondOperand = "";
+let divide = (firstNumber, secondNumber) => secondNumber / firstNumber;
+const firstScreen = document.querySelector('.firstScreen');
+const secondScreen = document.querySelector('.secondScreen');
+const numberBtn = document.querySelectorAll('[data-number]');
+const operatorBtn = document.querySelectorAll('[data-operator]');
+const clearBtn = document.querySelector('[data-clear]');
+const deleteBtn = document.querySelector('[data-delete]');
+const decimalBtn = document.querySelector('[data-decimal]');
+const equalBtn = document.querySelector('[data-equal]');
+
+
 
 function operate (firstNumber, operator, secondNumber) {
     if(operator === "+") {
@@ -19,11 +24,42 @@ function operate (firstNumber, operator, secondNumber) {
         return subtract(firstNumber, secondNumber);
     } else if(operator === "*") {
         return multiply(firstNumber, secondNumber);
-    } else if(operator === "/") {
+    } else if(operator === "÷") {
         return divide(firstNumber, secondNumber);
     } else {
         return "ERROR"
     }
 }
 
+numberBtn.forEach(numberBtn => {
+    numberBtn.addEventListener('click', (data) => {
+        resetScreen();
+        secondScreen.textContent += numberBtn.textContent;
+        firstOperand = secondScreen.textContent;
+    })
+});
 
+operatorBtn.forEach(operatorBtn => {
+    operatorBtn.addEventListener('click', (data) => {
+        operator = operatorBtn.textContent;
+        firstScreen.textContent += secondScreen.textContent + (" " + operator + " ");
+        secondOperand = secondScreen.textContent; 
+        
+    })
+})
+
+equalBtn.addEventListener('click', () => {
+    resetScreen();
+    let a = parseInt(firstOperand);
+    let b = parseInt(secondOperand);
+    firstScreen.textContent = b + " " + operator + " " + a + " " + "=";
+    secondScreen.textContent = operate(a, operator, b); 
+})
+
+
+function resetScreen() {
+    if (firstScreen.textContent !== "" && !shouldReset) {
+    shouldReset = true
+    secondScreen.textContent = ""
+    }
+}
